2c725666c6ce77f2f4f209a9bc9a89aafe3c3735
byungtak <>
2018-11-25 07:10:05 +0900

    add design on weeklytask viewcontroller

---
+++ TT_iOS/TT_iOS/Model/Weekly.swift
@@ -0,0 +1,15 @@
+//
+//  Weekly.swift
+//  TT_iOS
+//
+//  Created by byungtak on 25/11/2018.
+//  Copyright © 2018 mash_up_tt. All rights reserved.
+//
+
+import Foundation
+
+
+struct Weekly {
+    let date: String
+    let day: String
+}
