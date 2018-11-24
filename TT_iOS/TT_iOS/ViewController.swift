8ff7881a80fca518e042632e6ce98c93d5077875
byungtak <>
2018-11-24 22:21:09 +0900

    initial commit

---
+++ TT_iOS/TT_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TT_iOS
+//
+//  Created by byungtak on 24/11/2018.
+//  Copyright © 2018 mash_up_tt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
